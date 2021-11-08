bde76728b644e6fa202741c8a4004a0b6b082944
pitu <ashishkumarvish.432@gmail.com>
2021-11-08 14:39:56 +0530

    peak and pop method

--- src/com/bridgelab/MyStack.java
+++ src/com/bridgelab/MyStack.java
@@ -19,6 +19,24 @@ public class MyStack {
         top = node;
 
     }
+    /* This method is used to return top element from stack */
+    public int peak() {
+        System.out.println("Top element is " +top.key);
+        return top.key;
+    }
+
+    /* This method is used to delete element from stack */
+    public void pop() {
+        if (top == null) {
+            System.out.println("Stack empty");
+
+        }
+        else {
+            MyNode temp = top;
+            System.out.println("Popped out element is" +temp.key);
+            top = top.next;
+        }
+    }
     /* this method is used to display the stack list */
     public void display() {
         MyNode tempNode = top;
--- src/com/bridgelab/StackMain.java
+++ src/com/bridgelab/StackMain.java
@@ -7,6 +7,16 @@ public class StackMain {
         stack.push(70);
         stack.push(30);
         stack.push(56);
+        // To perform peak and pop till list is empty
+        stack.peak();
+        stack.pop();
+        stack.display();
+        stack.peak();
+        stack.pop();
+        stack.display();
+        stack.peak();
+        stack.pop();
+        stack.display();
 
     }
 }
